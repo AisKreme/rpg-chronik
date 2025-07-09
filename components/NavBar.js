5dc48b8415d76b2b57cd13f520761774b20c0b85
Maik <aiskreme@web.de>
2025-07-09 15:11:20 +0200

    Update Navbar

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,6 +1,7 @@
 
 import Link from 'next/link'
 
+export default function NavBar() {
   return (
     <nav className="flex gap-4 p-4 bg-parchment border-b border-ink">
       <Link href="/chronik">Chronik</Link>
